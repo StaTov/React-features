6533c809182e21a55d5ea8240cc25ba81783a3f4
StaTov <tovchfamily@gmail.com>
2023-11-10 02:40:24 +0300

    create infinity pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  useEffect, useRef, useState,
+} from 'react';
+import axios from 'axios';
+
+interface IData {
+  albumId: number,
+  id: number,
+  title: string,
+  url: string,
+  thumbnailUrl: string
+}
 
 function App() {
+  const page = useRef(1);
+  const totalCount = useRef(0);
+  const [photo, setPhoto] = useState<IData[]>([]);
+  const [fetching, setFetching] = useState(true);
+  useEffect(() => {
+    if (fetching) {
+      axios.get(`https://jsonplaceholder.typicode.com/photos?_limit=10&_page=${page.current}`)
+        .then((res) => {
+          totalCount.current = res.headers['x-total-count'];
+          setPhoto((prev) => [...prev, ...res.data]);
+        })
+        .finally(() => {
+          page.current += 1;
+          setFetching(false);
+        });
+    }
+  }, [fetching]);
+  const scrollHandler = () => {
+    const { documentElement } = document;
+    if ((documentElement.scrollHeight - (documentElement.scrollTop + window.innerHeight) < 100)
+      && totalCount.current > photo.length) {
+      setFetching(true);
+    }
+  };
+  useEffect(() => {
+    document.addEventListener('scroll', scrollHandler);
+    return () => document.removeEventListener('scroll', scrollHandler);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {photo.map((e) => (
+        <div key={e.id}>
+          <img src={e.thumbnailUrl} alt="" />
+        </div>
+      ))}
+    </>
   );
 }
 
