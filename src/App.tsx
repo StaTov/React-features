6c0ab3b35ffc7ee4b9bf5ba11a89c103cdc4d9a0
StaTov <tovchfamily@gmail.com>
2023-12-16 12:04:41 +0300

    empty dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import { useModal } from './hook/useModal';
 import './style.css'
-import Modal from './UI/Modal/Modal';
 
 const App = () => {
-  const [isOpen, openModal, closeModal] = useModal()
+
   return (
     <div className="container">
-      <button onClick={openModal}>open</button>
-      {isOpen &&
-        <Modal
-          onClose={closeModal}>
-          <div>Modal</div>
-        </Modal>}
     </div >)
 }
 export default App;
